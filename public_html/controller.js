912d61d1322b0bdfc17e19d95f778f487e9e8b1f
Thorsten Limbach <thorsten.limbach@ymail.com>
2021-01-02 22:56:03 +0100

    .

--- public_html/controller.js
+++ public_html/controller.js
@@ -6,25 +6,27 @@
 
 var isIPhone = false;
 $(document).ready(
-        function () {
-            console.log(window.location.href);
-            var apiKey = window.location.href.substring(window.location.href.length - 32, window.location.href.length);
-            console.log("api Key=" + apiKey);
+	function() {
+		console.log(window.location.href);
+		var apiKey = window.location.href.substring(window.location.href.length - 32, window.location.href.length);
+		console.log("api Key=" + apiKey);
 
-            new GuiController(apiKey).init();
+		isIPhone = $('body').innerWidth()<800;
 
-        });
+		new GuiController(apiKey).init();
+
+	});
 
 function FilterSelectList(selectListId, filterId) {
-    var filter = $("#" + filterId).val();
-    filter = filter.toUpperCase();
+	var filter = $("#" + filterId).val();
+	filter = filter.toUpperCase();
 
-    var options = $("#" + selectListId + " li");
-    for (var i = 0; i < options.length; i++) {
-        if (options[i].text.toUpperCase().indexOf(filter) < 0)
-            $(options[i]).css("display", "none");
-        else
-            $(options[i]).css("display", "block");
-    }
+	var options = $("#" + selectListId + " li");
+	for (var i = 0; i < options.length; i++) {
+		if (options[i].text.toUpperCase().indexOf(filter) < 0)
+			$(options[i]).css("display", "none");
+		else
+			$(options[i]).css("display", "block");
+	}
 }
 ;
